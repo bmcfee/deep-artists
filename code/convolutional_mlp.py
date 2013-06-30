a4dad1e316171b633b35da69da6842bec9fd021f
Nicola Montecchio <echonest@dyn-209-2-211-185.dyn.columbia.edu>
2013-06-30 10:37:40 -0400

    save model, ctrl-c stop

--- code/convolutional_mlp.py
+++ code/convolutional_mlp.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
 """This tutorial introduces the LeNet5 neural network architecture
 using Theano.  LeNet5 is a convolutional neural network, good for
 classifying images. This tutorial shows how to build the architecture,
@@ -26,7 +30,7 @@ import gzip
 import os
 import sys
 import time
-
+import sys
 import numpy
 
 import theano
@@ -257,68 +261,72 @@ def evaluate_lenet5(learning_rate=0.02, n_epochs=100,
     epoch = 0
     done_looping = False
 
-    while (epoch < n_epochs) and (not done_looping):
-        epoch = epoch + 1
-        for minibatch_index in xrange(n_train_batches):
+    try:
+        while (epoch < n_epochs) and (not done_looping):
+            epoch = epoch + 1
+            for minibatch_index in xrange(n_train_batches):
 
-            iter = (epoch - 1) * n_train_batches + minibatch_index
+                iter = (epoch - 1) * n_train_batches + minibatch_index
 
-            if iter % 100 == 0:
-                print 'training @ iter = ', iter
-            cost_ij = train_model(minibatch_index)
+                if iter % 100 == 0:
+                    print 'training @ iter = ', iter
+                cost_ij = train_model(minibatch_index)
 
-            if (iter + 1) % validation_frequency == 0:
+                if (iter + 1) % validation_frequency == 0:
 
-                # compute zero-one loss on validation set
-                validation_losses = [validate_model(i) for i
-                                     in xrange(n_valid_batches)]
-                this_validation_loss = numpy.mean(validation_losses)
-                print('epoch %i, minibatch %i/%i, validation error %f %%' % \
-                      (epoch, minibatch_index + 1, n_train_batches, \
-                       this_validation_loss * 100.))
+                    # compute zero-one loss on validation set
+                    validation_losses = [validate_model(i) for i
+                                         in xrange(n_valid_batches)]
+                    this_validation_loss = numpy.mean(validation_losses)
+                    print('epoch %i, minibatch %i/%i, validation error %f %%' % \
+                          (epoch, minibatch_index + 1, n_train_batches, \
+                           this_validation_loss * 100.))
 
-                # if we got the best validation score until now
-                if this_validation_loss < best_validation_loss:
+                    # if we got the best validation score until now
+                    if this_validation_loss < best_validation_loss:
 
-                    #improve patience if loss improvement is good enough
-                    if this_validation_loss < best_validation_loss *  \
-                       improvement_threshold:
-                        patience = max(patience, iter * patience_increase)
+                        #improve patience if loss improvement is good enough
+                        if this_validation_loss < best_validation_loss *  \
+                           improvement_threshold:
+                            patience = max(patience, iter * patience_increase)
 
-                    # save best validation score and iteration number
-                    best_validation_loss = this_validation_loss
-                    best_iter = iter
+                        # save best validation score and iteration number
+                        best_validation_loss = this_validation_loss
+                        best_iter = iter
 
-                    # test it on the test set
-                    test_losses = [test_model(i) for i in xrange(n_test_batches)]
-                    test_score = numpy.mean(test_losses)
-                    print(('     epoch %i, minibatch %i/%i, test error of best '
-                           'model %f %%') %
-                          (epoch, minibatch_index + 1, n_train_batches,
-                           test_score * 100.))
+                        # test it on the test set
+                        test_losses = [test_model(i) for i in xrange(n_test_batches)]
+                        test_score = numpy.mean(test_losses)
+                        print(('     epoch %i, minibatch %i/%i, test error of best '
+                               'model %f %%') %
+                              (epoch, minibatch_index + 1, n_train_batches,
+                               test_score * 100.))
 
-            if patience <= iter:
-                done_looping = True
-                break
+                if patience <= iter:
+                    done_looping = True
+                    break
 
-    end_time = time.clock()
-    print('Optimization complete.')
-    print('Best validation score of %f %% obtained at iteration %i,'\
-          'with test performance %f %%' %
-          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
-    print >> sys.stderr, ('The code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+        end_time = time.clock()
+        print('Optimization complete.')
+        print('Best validation score of %f %% obtained at iteration %i,'\
+              'with test performance %f %%' %
+              (best_validation_loss * 100., best_iter + 1, test_score * 100.))
+        print >> sys.stderr, ('The code for file ' +
+                              os.path.split(__file__)[1] +
+                              ' ran for %.2fm' % ((end_time - start_time) / 60.))
+    except KeyboardInterrupt:
+        print 'manually stopped'
 
     with open('model_output.pkl', 'w') as out:
-        cPickle.dump({'l0' : layer0,
-                      'l2' : layer2,
-                      'l3' : layer3})
-
+        # cPickle.dump({'l0' : layer0,
+        #               'l2' : layer2,
+        #               'l3' : layer3}, out)
+        cPickle.dump({'l0' : layer0.W.eval()}, out)
 
 
 if __name__ == '__main__':
-    evaluate_lenet5(dataset='artist_blah.npy')
+    dsfile = sys.argv[1]
+    evaluate_lenet5(dataset=dsfile)
 
 
 def experiment(state, channel):
